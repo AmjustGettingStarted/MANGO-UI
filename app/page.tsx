d48b7d5891f38c58da6e5a838b7b8b7fadde3942
AmjustGettingStarted <gamerhmv2050@gmail.com>
2026-08-29 16:14:20 +0530

    feat(features): add features page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Navbar from '@/components/landing/navbar';
 import Hero from '@/components/landing/hero';
 import Stats from '@/components/landing/stats';
-// import Features from '@/components/landing/features';
+import Features from '@/components/landing/features';
 // import AnimatedBento from '@/components/landing/animated-bento';
 // import ComponentsBento from '@/components/landing/component-bento';
 // import TemplateBento from '@/components/landing/template-bento';
@@ -14,7 +14,7 @@ export default function App() {
       <Navbar />
       <Hero />
       <Stats />
-      {/* <Features /> */}
+      <Features />
       {/* <AnimatedBento /> */}
       {/* <ComponentsBento /> */}
       {/* <TemplateBento /> */}
