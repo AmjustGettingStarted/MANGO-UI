49c612e9058cd4bb7625b137978eeeba576f4511
AmjustGettingStarted <gamerhmv2050@gmail.com>
2026-08-29 16:04:52 +0530

    feat(navbar): add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,25 @@
 import Navbar from '@/components/landing/navbar';
-import Hero from '@/components/landing/hero';
-import Stats from '@/components/landing/stats';
-import Features from '@/components/landing/features';
-import AnimatedBento from '@/components/landing/animated-bento';
-import ComponentsBento from '@/components/landing/component-bento';
-import TemplateBento from '@/components/landing/template-bento';
-import Testimonial from '@/components/landing/testimonial';
-import Footer from '@/components/landing/footer';
+// import Hero from '@/components/landing/hero';
+// import Stats from '@/components/landing/stats';
+// import Features from '@/components/landing/features';
+// import AnimatedBento from '@/components/landing/animated-bento';
+// import ComponentsBento from '@/components/landing/component-bento';
+// import TemplateBento from '@/components/landing/template-bento';
+// import Testimonial from '@/components/landing/testimonial';
+// import Footer from '@/components/landing/footer';
 
 export default function App() {
   return (
     <main className="dark min-h-screen overflow-x-hidden bg-[#101010]">
       <Navbar />
-      <Hero />
+      {/* <Hero />
       <Stats />
       <Features />
       <AnimatedBento />
       <ComponentsBento />
       <TemplateBento />
       <Testimonial />
-      <Footer />
+      <Footer /> */}
     </main>
   );
 }
