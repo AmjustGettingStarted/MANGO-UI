4203972b6766d5905ea75d851338d98e6f6ab48c
AmjustGettingStarted <gamerhmv2050@gmail.com>
2026-08-30 14:59:09 +0530

    feat(page): add central home fallback page for placeholder routes

---
+++ app/home/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div className='w-full h-screen flex items-center justify-center'>
+            <h1 className='text-yellow-500 text-7xl font-extrabold'>Coming soon ...</h1>
+        </div>
+    )
+}
+
+export default Page
